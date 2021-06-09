a1bd35a06cece0a75ea8959705effd05d47b8faa
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-06-09 13:43:03 +0200

    Use Subject for online check box too

--- src/app/components/tv-show-add/tv-show-add.component.ts
+++ src/app/components/tv-show-add/tv-show-add.component.ts
@@ -3,6 +3,7 @@ import {TvShow} from "../../models/TvShow";
 import {TvShowService} from "../../services/tv-show.service";
 import {Router} from "@angular/router";
 import {Item} from "../../models/Item";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-tv-show-add',
@@ -13,15 +14,22 @@ export class TvShowAddComponent implements OnInit {
 
   tvShows: TvShow[] = [];
   online: boolean = false;
+  private title: string = "";
+  online$ = new Subject<boolean>();
 
   constructor(private tvShowService: TvShowService, private router: Router) {
   }
 
   ngOnInit(): void {
+    this.online$.subscribe(value => {
+      this.online = value;
+      this.searchTvShow(this.title);
+    });
   }
 
-  searchTvShow(title: string, online: boolean) {
-    this.tvShowService.lookupTvShow(title, online).then(tvShowResponse => this.tvShows = tvShowResponse);
+  searchTvShow(title: string) {
+    this.title = title;
+    this.tvShowService.lookupTvShow(this.title, this.online).then(tvShowResponse => this.tvShows = tvShowResponse);
   }
 
   addToWatchListAndGoToDetail(tvShow: Item) {
