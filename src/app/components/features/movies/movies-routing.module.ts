598dd031f837eed17bf04eb89559dda4bfce7e22
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-06-11 09:45:41 +0200

    Implements Movies Routing Module

--- src/app/components/features/movies/movies-routing.module.ts
+++ src/app/components/features/movies/movies-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MovieAddComponent} from "./movie-add/movie-add.component";
+import {MovieDetailComponent} from "./movie-detail/movie-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component: MovieAddComponent},
+  {path: ':id', component: MovieDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/features/movies/movies.module.ts
+++ src/app/components/features/movies/movies.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import {SharedModule} from "../../shared/shared.module";
 import {MovieAddComponent} from "./movie-add/movie-add.component";
 import {MovieDetailComponent} from "./movie-detail/movie-detail.component";
+import {MoviesRoutingModule} from "./movies-routing.module";
 
 
 
 @NgModule({
   declarations: [MovieAddComponent, MovieDetailComponent],
   imports: [
-    SharedModule
+    SharedModule,
+    MoviesRoutingModule
   ]
 })
 export class MoviesModule { }
