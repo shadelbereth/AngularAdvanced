4693ccc8cf6dc3c7e2af835981839205beb2b8bd
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-06-11 14:47:31 +0200

    Implement Preload Strategy Service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,21 @@ import {MovieHomeComponent} from "./components/pages/movie-home/movie-home.compo
 import {TvShowHomeComponent} from "./components/pages/tv-show-home/tv-show-home.component";
 import {HomeComponent} from "./components/core/home/home.component";
 import {LoginComponent} from "./components/core/login/login.component";
+import {PreloadStrategyService} from "./components/core/preload-strategy.service";
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'tvshow/home', component: TvShowHomeComponent},
-  {path: 'tvshow', loadChildren: () => import('./components/features/tv-shows/tvshows.module').then(m => m.TVShowsModule)},
+  {path: 'tvshow', loadChildren: () => import('./components/features/tv-shows/tvshows.module').then(m => m.TVShowsModule),
+    data: {preload: true}},
   {path: 'movie/home', component: MovieHomeComponent},
   {path: 'movie', loadChildren: () => import('./components/features/movies/movies.module').then(m => m.MoviesModule)},
   {path: '**', redirectTo:'home'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadStrategyService})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
--- src/app/components/core/preload-strategy.service.ts
+++ src/app/components/core/preload-strategy.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {PreloadingStrategy, Route} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class PreloadStrategyService {
+export class PreloadStrategyService implements PreloadingStrategy{
 
   constructor() { }
+
+  preload(route: Route, load: () => Observable<any>): Observable<any> {
+    if (route.data && route.data['preload']) {
+      return load();
+    } else {
+      return new Observable(subscriber => subscriber.next(null));
+    }
+  }
 }
