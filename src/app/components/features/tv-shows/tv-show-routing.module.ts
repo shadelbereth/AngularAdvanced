9ee41c46babd7834925d3c4a15249b5103a1a149
Sophie.Roch <sophie.roch@economie.fgov.be>
2021-06-11 09:48:50 +0200

    Implement TV Show Routing Module

--- src/app/components/features/tv-shows/tv-show-routing.module.ts
+++ src/app/components/features/tv-shows/tv-show-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TvShowAddComponent} from "./tv-show-add/tv-show-add.component";
+import {TvShowDetailComponent} from "./tv-show-detail/tv-show-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component: TvShowAddComponent},
+  {path: ':id', component: TvShowDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/components/features/tv-shows/tvshows.module.ts
+++ src/app/components/features/tv-shows/tvshows.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import {SharedModule} from "../../shared/shared.module";
 import {TvShowAddComponent} from "./tv-show-add/tv-show-add.component";
 import {TvShowDetailComponent} from "./tv-show-detail/tv-show-detail.component";
+import {TvShowRoutingModule} from "./tv-show-routing.module";
 
 
 
 @NgModule({
   declarations: [TvShowAddComponent, TvShowDetailComponent],
   imports: [
-    SharedModule
+    SharedModule,
+    TvShowRoutingModule
   ]
 })
 export class TVShowsModule { }
